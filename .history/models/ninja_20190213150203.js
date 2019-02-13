aba0fb65204937e26decdf1b7eca1abb027c5f91
Sebastian <votex999@gmail.com>
2019-02-13 15:05:51 +0100

    adding Schema to model in mongodb

---
+++ .history/models/ninja_20190213150203.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+//create ninja Schema & model
+const NinjaSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Name field is required']
+  }
+})
